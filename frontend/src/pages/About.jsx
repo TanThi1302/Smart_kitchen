85443573ce71c2f7d03cbfb349c48494a4065258
thanhhbao <thanhhbao4123@gmail.com>
2025-10-26 23:32:31 +0700

    Add About and Promotion pages

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>Giới thiệu</h1>
+      <p>Đây là trang giới thiệu về công ty hoặc sản phẩm.</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ frontend/src/pages/Promotion.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Promotion = () => {
+  return (
+    <div>
+      <h1>Chương trình khuyến mãi</h1>
+      <p>Thông tin về các chương trình ưu đãi hiện tại.</p>
+    </div>
+  );
+};
+
+export default Promotion;
